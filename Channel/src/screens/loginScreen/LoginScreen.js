b2e202fb744af01a43eee389b85ef25e8999a117
Parakh <parakhjain2301@gmail.com>
2023-01-23 13:19:46 +0530

    added app icon

--- Channel/src/screens/loginScreen/LoginScreen.js
+++ Channel/src/screens/loginScreen/LoginScreen.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Button, Text } from 'react-native';
+import { Button, Linking, Text } from 'react-native';
 
 const LoginScreen  = () => {
     console.log('Inside Login Screen');
     return (
-        <Button title='Login Via Space' />
+        <Button title='Login Via Space' onPress={()=>Linking.openURL('https://auth.intospace.io')}/>
         // <Text>helloo</Text>
     );
 }
