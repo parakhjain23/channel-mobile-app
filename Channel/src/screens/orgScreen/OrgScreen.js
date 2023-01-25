9ae5cecfce9b300c9bc3b2d650d74f33768a7d39
Parakh <parakhjain2301@gmail.com>
2023-01-25 13:52:33 +0530

    added fonts for ios

--- Channel/src/screens/orgScreen/OrgScreen.js
+++ Channel/src/screens/orgScreen/OrgScreen.js
@@ -7,18 +7,22 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const OrgCard = ({item}) => {
   return (
-    <TouchableOpacity>
-      <View style={{flexDirection: 'row', alignItems: 'center', margin: 10}}>
-        <Image
-          source={{uri: item.icon}}
-          style={{height: 40, width: 40, marginRight: 10}}
-        />
-        <Text>{item.name}</Text>
-        <Icon name='chevron-right'/>
+    <TouchableOpacity style={{borderWidth:.5,borderColor:'gray',borderRadius:5,marginVertical:'1%'}}>
+      <View style={{flexDirection: 'row', alignItems: 'center',justifyContent:'space-between',padding:8}}>
+        <View style={{flexDirection: 'row',alignItems:'center'}}>
+          <Image
+            source={{uri: item.icon}}
+            style={{height: 40, width: 40, marginRight: 10}}
+          />
+          <Text>{item.name}</Text>
+        </View>
+        <View>
+          <Icon name="chevron-right" />
+        </View>
       </View>
     </TouchableOpacity>
   );
@@ -32,6 +36,10 @@ const OrgScreen = () => {
     },
     {
       name: 'Walkover',
+      icon: 'https://resources.intospace.io/cdn-cgi/image/width=96/gMv5N0EtECmu4Fa9_6b30b481-85e1-4ec6-9265-8464ea680f8f',
+    },
+    {
+      name: 'Channel',
       icon: 'https://resources.intospace.io/cdn-cgi/image/width=96/gMv5N0EtECmu4Fa9_6b30b481-85e1-4ec6-9265-8464ea680f8f',
     },
   ];
