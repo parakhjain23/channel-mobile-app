b96c10192e6157d8a755377c2fc97db454c8b0fe
rudrakachhawa <rudrakshkachhawa@gmail.com>
2023-01-27 12:35:48 +0530

    changes in userReducer

--- Channel/src/redux/reducers/user/UserInfo.js
+++ Channel/src/redux/reducers/user/UserInfo.js
@@ -1,16 +1,20 @@
+import * as Actions from "../../Enums";
+
+
 const initialState = {
   user: null,
   guest: false,
   isLoading: true,
   isAuthticated: false,
   isSignedIn: false,
-  accessToken: null
+  accessToken: null,
+  orgId:null
 };
 
 export function userInfoReducer(state = initialState, action) {
   switch (action.type) {
-    case "SAVETOKEN":
-      return {...state,accessToken:action.payload,isSignedIn:true}
+    case Actions.SAVETOKEN:
+      return {...state,accessToken:action.accessToken,orgId : action.orgId,isSignedIn:true}
     default:
       return state;
   }
