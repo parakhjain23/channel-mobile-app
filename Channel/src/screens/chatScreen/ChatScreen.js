43082132267fde47d33fa54c7c9d51bb49b8e1f6
Parakh <parakhjain2301@gmail.com>
2023-02-09 15:32:44 +0530

    swipable for android

--- Channel/src/screens/chatScreen/ChatScreen.js
+++ Channel/src/screens/chatScreen/ChatScreen.js
@@ -10,7 +10,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import {Swipeable} from 'react-native-gesture-handler';
+import {GestureHandlerRootView, Swipeable} from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {connect} from 'react-redux';
@@ -47,7 +47,7 @@ const ChatCard = ({
   chatState,
   setreplyOnMessage,
   setrepliedMsgDetails,
-  // image = 'https://t4.ftcdn.net/jpg/05/11/55/91/360_F_511559113_UTxNAE1EP40z1qZ8hIzGNrB0LwqwjruK.jpg',
+  image = 'https://t4.ftcdn.net/jpg/05/11/55/91/360_F_511559113_UTxNAE1EP40z1qZ8hIzGNrB0LwqwjruK.jpg',
 }) => {
   const [optionsVisible, setOptionsVisible] = useState(false);
   const swipeableRef = useRef(null);
@@ -76,51 +76,56 @@ const ChatCard = ({
   };
   return (
     <>
-      {optionsVisible && (
-        <View style={styles.optionsContainer}>
-          <TouchableOpacity
-            onPress={() => {
-              setOptionsVisible(false),
-                deleteMessageAction(userInfoState?.accessToken, chat?._id);
-            }}>
-            <Text style={styles.option}>Delete</Text>
-          </TouchableOpacity>
-        </View>
-      )}
       {!chat?.isActivity ? (
-        <TouchableOpacity onLongPress={onLongPress}>
-          <Swipeable
-            ref={swipeableRef}
-            renderLeftActions={LeftSwipeActions}
-            onSwipeableLeftOpen={swipeFromLeftOpen}>
-            <View
-              style={[
-                styles.container,
-                sentByMe ? styles.sentByMe : styles.received,
-              ]}>
-              {/* {sentByMe ? null : (
-          <Image source={{uri: image}} style={styles.avatar} />
-        )} */}
-              <View style={styles.textContainer}>
-                <Text style={styles.nameText}>{SenderName}</Text>
-                {parentId != null && (
-                  <View style={styles.repliedContainer}>
-                    <Text>
-                      {
-                        chatState?.data[chat.teamId]?.parentMessages[parentId]
-                          ?.content
-                      }
-                    </Text>
-                  </View>
+        <TouchableOpacity onLongPress={sentByMe ? onLongPress : null}>
+          <GestureHandlerRootView>
+            <Swipeable
+              ref={swipeableRef}
+              renderLeftActions={LeftSwipeActions}
+              onSwipeableLeftOpen={swipeFromLeftOpen}>
+              <View
+                style={[
+                  styles.container,
+                  sentByMe ? styles.sentByMe : styles.received,
+                ]}>
+                {sentByMe ? null : (
+                  <Image source={{uri: image}} style={styles.avatar} />
                 )}
-                <Text style={styles.messageText}>{chat?.content}</Text>
+                <View style={styles.textContainer}>
+                  <Text style={styles.nameText}>{SenderName}</Text>
+                  {parentId != null && (
+                    <View style={styles.repliedContainer}>
+                      <Text>
+                        {
+                          chatState?.data[chat.teamId]?.parentMessages[parentId]
+                            ?.content
+                        }
+                      </Text>
+                    </View>
+                  )}
+                  <Text style={styles.messageText}>{chat?.content}</Text>
+                </View>
+                <Text style={styles.timeText}>{time}</Text>
+                {sentByMe ? (
+                  <Image source={{uri: image}} style={styles.avatar} />
+                ) : null}
               </View>
-              <Text style={styles.timeText}>{time}</Text>
-              {/* {sentByMe ? (
-          <Image source={{uri: image}} style={styles.avatar} />
-        ) : null} */}
-            </View>
-          </Swipeable>
+              <View style={sentByMe ? styles.sentByMe : styles.received}>
+                {optionsVisible && (
+                  <TouchableOpacity
+                    onPress={() => {
+                      setOptionsVisible(false),
+                        deleteMessageAction(
+                          userInfoState?.accessToken,
+                          chat?._id,
+                        );
+                    }}>
+                    <Text>Delete</Text>
+                  </TouchableOpacity>
+                )}
+              </View>
+            </Swipeable>
+          </GestureHandlerRootView>
         </TouchableOpacity>
       ) : (
         <AddRemoveJoinedMsg
@@ -130,124 +135,6 @@ const ChatCard = ({
         />
       )}
     </>
-    // <Swipeable
-    //   ref={swipeableRef}
-    //   renderLeftActions={LeftSwipeActions}
-    //   onSwipeableLeftOpen={swipeFromLeftOpen}>
-    //   <View
-    //     style={[
-    //       styles.container,
-    //       sentByMe ? styles.sentByMe : styles.received,
-    //     ]}>
-    //     {/* {sentByMe ? null : (
-    //       <Image source={{uri: image}} style={styles.avatar} />
-    //     )} */}
-    //     <View style={styles.textContainer}>
-    //       <Text style={styles.nameText}>{SenderName}</Text>
-    //       {parentId != null && (
-    //         <View style={styles.repliedContainer}>
-    //           <Text>
-    //             {
-    //               chatState?.data[chat.teamId]?.parentMessages[parentId]
-    //                 ?.content
-    //             }
-    //           </Text>
-    //         </View>
-    //       )}
-    //       <Text style={styles.messageText}>{chat?.content}</Text>
-    //     </View>
-    //     <Text style={styles.timeText}>{time}</Text>
-    //     {/* {sentByMe ? (
-    //       <Image source={{uri: image}} style={styles.avatar} />
-    //     ) : null} */}
-    //   </View>
-    // </Swipeable>
-  );
-};
-const RenderChatCard = ({
-  chat,
-  userInfoState,
-  orgState,
-  deleteMessageAction,
-  chatState,
-  setreplyOnMessage,
-  setrepliedMsgDetails,
-}) => {
-  const [optionsVisible, setOptionsVisible] = useState(false);
-  const onLongPress = () => {
-    setOptionsVisible(true);
-  };
-  const parentId = chat?.parentId;
-  const date = new Date(chat.updatedAt);
-  const FlexAlign =
-    chat?.senderId == userInfoState?.user?.id ? 'flex-end' : 'flex-start';
-  const SenderName =
-    chat?.senderId == userInfoState?.user?.id
-      ? 'You'
-      : orgState?.userIdAndNameMapping[chat?.senderId];
-  return (
-    <TouchableOpacity onLongPress={onLongPress}>
-      {optionsVisible && (
-        <View style={styles.optionsContainer}>
-          <TouchableOpacity
-            onPress={() => {
-              setOptionsVisible(false),
-                deleteMessageAction(userInfoState?.accessToken, chat?._id);
-            }}>
-            <Text style={styles.option}>Delete</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              setOptionsVisible(false);
-              setrepliedMsgDetails(chat);
-              setreplyOnMessage(true);
-            }}>
-            <Text style={styles.option}>reply</Text>
-          </TouchableOpacity>
-        </View>
-      )}
-      {!chat?.isActivity ? (
-        <View style={styles.messageContainer}>
-          <View style={[styles.senderName, {alignSelf: FlexAlign}]}>
-            <Text>{SenderName}</Text>
-          </View>
-          <View
-            style={[
-              styles.message,
-              {
-                alignSelf: FlexAlign,
-                borderWidth: 1,
-                // borderColor: 'gray',
-                borderRadius: 10,
-                padding: 8,
-              },
-            ]}>
-            {parentId != null && (
-              <View>
-                <Text>
-                  {
-                    chatState?.data[chat.teamId]?.parentMessages[parentId]
-                      ?.content
-                  }
-                </Text>
-              </View>
-            )}
-            <Text>{chat?.content}</Text>
-            <View style={styles.timeContainer}>
-              <Text style={styles.time}>
-                {date.getHours() + ':' + date.getMinutes()}
-              </Text>
-            </View>
-          </View>
-        </View>
-      ) : (
-        <AddRemoveJoinedMsg
-          senderName={SenderName}
-          content={chat?.content}
-          orgState={orgState}
-        />
-      )}
-    </TouchableOpacity>
   );
 };
 const ListFooterComponent = () => {
@@ -393,39 +280,11 @@ const mapDispatchToProps = dispatch => {
 };
 export default connect(mapStateToProps, mapDispatchToProps)(ChatScreen);
 const styles = StyleSheet.create({
-  messageContainer: {
-    margin: 10,
-  },
   repliedContainer: {
     padding: 5,
     backgroundColor: '#d9d9d9',
     borderRadius: 5,
     marginBottom: 4,
-  },
-  senderName: {
-    alignSelf: 'flex-start',
-  },
-  message: {
-    width: '70%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    flexWrap: 'wrap',
-  },
-  timeContainer: {
-    justifyContent: 'flex-end',
-  },
-  time: {
-    fontSize: 10,
-  },
-  optionsContainer: {
-    // position: 'relative',
-    flex: 1,
-    flexDirection: 'row',
-    padding: 8,
-    borderRadius: 10,
-    width: 100,
-    backgroundColor: 'red',
-    alignSelf: 'flex-end',
   },
   option: {
     margin: 8,
