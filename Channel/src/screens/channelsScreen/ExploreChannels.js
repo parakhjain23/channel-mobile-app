8a8bdab1ca557db43b481e3d2f6fe044d58d3a49
Parakh <parakhjain2301@gmail.com>
2023-02-15 20:02:33 +0530

    explore channels props are not comming

--- Channel/src/screens/channelsScreen/ExploreChannels.js
+++ Channel/src/screens/channelsScreen/ExploreChannels.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
 
-const ExploreChannels = () => {
-    return (
-        <></>
-    )
-}
+const ExploreChannels = ({route}) => {
+    const {props} = route?.params;
+    console.log(props);
+  const [channels, setChannels] = useState([]);
+  console.log(props?.userInfoState?.accessToken,props?.orgsState?.currenOrgId);
+  async function fetchAllChannels() {
+    const response = await fetch(`https://api.intospace.io/chat//team?$paginate=false&orgId=${props?.orgsState?.currenOrgId}&isArchived=false&includeUsers=false&$limit=50`,{
+        method:'GET',
+        headers:{
+            "Authorization": props?.userInfoState?.accessToken
+        }
+    })
+    const result = await response.json()
+    // console.log(result);
+  }
+  useEffect(() => {
+    fetchAllChannels();
+  }, []);
+
+  return <></>;
+};
 export default ExploreChannels;
--- Channel/src/screens/channelsScreen/NoChannelsFound.js
+++ Channel/src/screens/channelsScreen/NoChannelsFound.js
@@ -1,23 +1,42 @@
+import { useNavigation } from '@react-navigation/native';
 import React, {useRef} from 'react';
 import {Button, Text, View} from 'react-native';
 import {Modalize} from 'react-native-modalize';
-import ExploreChannels from './ExploreChannels';
 
-const NoChannelsFound = props => {
+const NoChannelsFound = ({modalizeRef,props}) => {
+  const navigation = useNavigation()
+  // const modalizeRef = useRef(null);
 
+  // const ExploreChannelModal = ({modalizeRef}) => {
+  //   console.log('in explore');
+  //   return (
+  //     <Modalize
+  //       scrollViewProps={{keyboardShouldPersistTaps: 'always'}}
+  //       ref={modalizeRef}
+  //       modalStyle={{top: '12%'}}>
+  //       <View style={{margin: 12}}>
+  //         <Text>hello</Text>
+  //       </View>
+  //     </Modalize>
+  //   );
+  // };
+  // const onOpen = () => {
+  //   console.log('in onopen');
+  //   modalizeRef?.current?.open();
+  // };
   return (
     <View style={{flex:1,justifyContent:'center'}}>
-      <View style={{alignSelf: 'center'}}>
+      <View style={{alignSelf: 'center', justifyContent: 'center'}}>
         <Text style={{fontSize: 18, textAlign: 'center'}}>
           No channels found
         </Text>
-        <Button title="Explore All Channels" onPress={onOpen} />
+        <Button title="Explore All Channels" onPress={()=>navigation.navigate('ExploreChannels',{props:props})} />
         <Button
           title="Create New Channel"
-          onPress={() => props?.modalizeRef?.current?.open()}
+          onPress={() => modalizeRef?.current?.open()}
         />
       </View>
-      <ExploreChannels />
+      {/* <ExploreChannelModal modalizeRef={modalizeRef} /> */}
     </View>
   );
 };
