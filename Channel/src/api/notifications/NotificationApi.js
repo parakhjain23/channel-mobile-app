24a393571ce6631304c428d14e3778ff32a474c6
rudrakachhawa <rudrakshkachhawa@gmail.com>
2023-02-20 11:26:46 +0530

    notification refactor

--- Channel/src/api/notifications/NotificationApi.js
+++ Channel/src/api/notifications/NotificationApi.js
@@ -13,7 +13,7 @@ const notificationApi = async (token, deviceId) => {
     var myHeaders = new Headers();
     myHeaders.append(
       'Authorization',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6ImFjY2VzcyJ9.eyJlbWFpbCI6InJ1ZHJha3Noa2FjaGhhd2FAZ21haWwuY29tIiwiaWF0IjoxNjc0NjM4NTc0LCJleHAiOjE3MDYxOTYxNzQsImF1ZCI6Imh0dHBzOi8veW91cmRvbWFpbi5jb20iLCJpc3MiOiJmZWF0aGVycyIsInN1YiI6ImdNdjVOMEV0RUNtdTRGYTkiLCJqdGkiOiI2ZWQxNWUwOC1lYTFkLTQwNDAtOWQ3Yi03ZjIyZDk5OGUwNTYifQ.JUQf6HMioyPdzRjpL9iqf4YEwZgykJLw-qAJvbV-2yk',
+      `${token}`,
     );
     myHeaders.append('Content-Type', 'application/json');
 
