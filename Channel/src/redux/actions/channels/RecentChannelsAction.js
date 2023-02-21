4821df1bb4a24c6ea4e0470e2809c9f85be54745
Parakh <parakhjain2301@gmail.com>
2023-02-21 19:11:51 +0530

    get recent channels success

---
+++ Channel/src/redux/actions/channels/RecentChannelsAction.js
@@ -0,0 +1,19 @@
+import { call, put } from 'redux-saga/effects';
+import { getRecenctChannelsApi } from '../../../api/channelsApi/GetRecentChannelsApi';
+import * as Actions from '../../Enums';
+
+export function* getRecentChannels({accessToken,orgId,userId}){
+    try {
+      var response = yield call(getRecenctChannelsApi,accessToken,orgId,userId)
+      yield put(getRecentChannelsSuccess(response))
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+export function getRecentChannelsSuccess(response){
+  return {
+    type: Actions.FETCH_RECENT_CHANNELS_SUCCESS,
+    recentChannels: response
+  }
+}
