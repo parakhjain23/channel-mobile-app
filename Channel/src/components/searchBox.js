97d4d8c7826a669f1a7ef5d5e4c2b9f0c36220e8
Parakh <parakhjain2301@gmail.com>
2023-03-17 15:36:10 +0530

    change search bar

--- Channel/src/components/searchBox.js
+++ Channel/src/components/searchBox.js
@@ -1,32 +1,46 @@
-import React from "react";
-import { SearchBar } from "@rneui/themed";
+import React from 'react';
+import {SearchBar} from '@rneui/themed';
+import {TextInput} from 'react-native-paper';
 
-const SearchBox = ({ searchValue, changeText, isSearchFocus,hits }) => {
-    
-    return <SearchBar
-        autoFocus={isSearchFocus}
-        lightTheme={false}
-        leftIconContainerStyle={{
-            marginLeft: 5,
-            marginRight: 1,
-          }}
-        containerStyle={{
-            backgroundColor: 'white',
-            borderColor: 'black',
-            paddingTop: 5,
-          }}
-        inputContainerStyle={{
-            backgroundColor: 'white',
-            paddingLeft: 1,
-          }}
-        placeholder='Search Channels'
-        onChangeText={changeText}
-        style={{
-            color: 'black',
-            margin: 0,
-          }}
-        value={searchValue}
+const SearchBox = ({searchValue, changeText, isSearchFocus, hits}) => {
+  return (
+    <TextInput
+      value={searchValue}
+      onChangeText={changeText}
+      label='Search Channels'
+      style={{backgroundColor: 'white', borderWidth: 1, borderColor: 'black',marginHorizontal:1}}
+      underlineStyle={{backgroundColor:'transparent'}}
+      underlineColorAndroid="transparent"
+      clearButtonMode="always"
+      placeholder="Eg: Walkover"
+      left={<TextInput.Icon icon="magnify" style={{justifyContent:'center'}}/>}
     />
-}
+    // <SearchBar
+    //   autoFocus={isSearchFocus}
+    //   lightTheme={false}
+    //   leftIconContainerStyle={{}}
+    //   containerStyle={{
+    //     backgroundColor: 'white',
+    //     borderColor: 'black',
+    //     paddingTop: 5,
+    //     marginHorizontal: 5,
+    //     borderRadius: 10,
+    //     borderWidth: 1,
+    //     borderColor: 'black',
+    //   }}
+    //   inputContainerStyle={{
+    //     backgroundColor: 'white',
+    //     paddingLeft: 1,
+    //   }}
+    //   placeholder="Search Channels"
+    //   onChangeText={changeText}
+    //   style={{
+    //     color: 'black',
+    //     margin: 0,
+    //   }}
+    //   value={searchValue}
+    // />
+  );
+};
 
-export default (SearchBox)
+export default SearchBox;
