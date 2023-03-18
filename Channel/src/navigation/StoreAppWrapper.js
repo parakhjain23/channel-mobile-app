fc489d468450dc702364dd23d2a11b9ba1a396f7
rudrakachhawa <rudrakshkachhawa@gmail.com>
2023-03-18 11:51:51 +0530

    merge with visitprofile branch

--- Channel/src/navigation/StoreAppWrapper.js
+++ Channel/src/navigation/StoreAppWrapper.js
@@ -21,8 +21,6 @@ const StoreAppWrapper = ({userInfoSate,orgsState}) => {
 
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener(state => {
-      // console.log('Connection type', state.type);
-      // console.log('Is connected?', state.isConnected);
       dispatch(networkStatus(state?.isConnected));
     });
     return () => {
