3bde0e68d0318ac33ddabe5d12c24a464508a7aa
Parakh <parakhjain2301@gmail.com>
2023-04-07 12:12:26 +0530

    mention upper lower text bug fix

--- Channel/src/screens/chatScreen/RenderTextWithLinks.js
+++ Channel/src/screens/chatScreen/RenderTextWithLinks.js
@@ -5,7 +5,7 @@ import {Linking} from 'react-native';
 import * as RootNavigation from '../../navigation/RootNavigation';
 import cheerio from 'cheerio';
 import InAppBrowser from 'react-native-inappbrowser-reborn';
-import { useTheme } from 'react-native-paper';
+import {useTheme} from 'react-native-paper';
 
 export const RenderTextWithLinks = ({
   text,
@@ -15,11 +15,13 @@ export const RenderTextWithLinks = ({
   searchUserProfileAction,
   userInfoState,
   colors,
-  sentByMe
+  sentByMe,
 }) => {
   const {dark} = useTheme();
-  const fontWeight = dark ? '700' : '900' 
-  const linkColor = sentByMe ? colors.sentByMeLinkColor : colors.recivedLinkColor
+  const fontWeight = dark ? '700' : '900';
+  const linkColor = sentByMe
+    ? colors.sentByMeLinkColor
+    : colors.recivedLinkColor;
   const urlRegex =
     /((?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+(?:#[\w\-])?(?:\?[^\s])?)/gi;
   function findKeyByValue(value) {
@@ -32,7 +34,11 @@ export const RenderTextWithLinks = ({
     return null;
   }
   function highlight(text, result, repliedContainer) {
-    const textColor = repliedContainer ? 'black' : sentByMe ? colors.sentByMeTextColor : colors?.textColor;
+    const textColor = repliedContainer
+      ? 'black'
+      : sentByMe
+      ? colors.sentByMeTextColor
+      : colors?.textColor;
     let A = orgState?.userIdAndDisplayNameMapping;
     text = text.replace(/@{1,2}(\w+)/g, (match, p1) => {
       return A[p1] ? `@${A[p1]}` : match;
@@ -68,9 +74,11 @@ export const RenderTextWithLinks = ({
         }
       }
       return (
-        <Text key={index} style={{color: textColor}}>
-          {part}
-        </Text>
+        <TouchableOpacity activeOpacity={1}>
+          <Text key={index} style={{color: textColor}}>
+            {part}
+          </Text>
+        </TouchableOpacity>
       );
     });
   }
@@ -127,7 +135,11 @@ export const RenderTextWithLinks = ({
             openLink(url);
           }}>
           <Text
-            style={{color: linkColor, textDecorationLine: 'underline',fontWeight:fontWeight}}>
+            style={{
+              color: linkColor,
+              textDecorationLine: 'underline',
+              fontWeight: fontWeight,
+            }}>
             {part}
           </Text>
         </TouchableOpacity>
