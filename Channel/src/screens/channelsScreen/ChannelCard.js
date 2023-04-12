307145a7a9283d082253d4692c07f7a824d05e63
rudrakachhawa <rudrakshkachhawa@gmail.com>
2023-04-12 14:18:44 +0530

    ripple effect on press of channel card

--- Channel/src/screens/channelsScreen/ChannelCard.js
+++ Channel/src/screens/channelsScreen/ChannelCard.js
@@ -1,12 +1,22 @@
 import {useTheme} from '@react-navigation/native';
 import React from 'react';
-import {Text, TouchableOpacity, View, Button} from 'react-native';
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  Button,
+  TouchableNativeFeedback,
+  StyleSheet,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {connect} from 'react-redux';
 import {fetchSearchedUserProfileStart} from '../../redux/actions/user/searchUserProfileActions';
 import {s, vs, ms, mvs} from 'react-native-size-matters';
 import {resetUnreadCountStart} from '../../redux/actions/channels/ChannelsAction';
 import * as RootNavigation from '../../navigation/RootNavigation';
+const TouchableItem =
+  Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
+
 const ChannelCard = ({item, navigation, props, resetUnreadCountAction}) => {
   const {colors} = useTheme();
   const Name =
@@ -35,102 +45,92 @@ const ChannelCard = ({item, navigation, props, resetUnreadCountAction}) => {
           ? true
           : false)
       : false);
-
+  const onPress = () => {
+    RootNavigation.navigate('Chat', {
+      chatHeaderTitle: Name,
+      teamId: item?._id,
+      channelType: item?.type,
+      userId:
+        item?.userIds[0] != props?.userInfoState?.user?.id
+          ? item?.userIds[0]
+          : item?.userIds[1],
+    });
+    props?.setActiveChannelTeamIdAction(item?._id);
+    props?.channelsState?.teamIdAndUnreadCountMapping?.[item?._id] > 0 &&
+      resetUnreadCountAction(
+        props?.orgsState?.currentOrgId,
+        props?.userInfoState?.user?.id,
+        item?._id,
+        props?.userInfoState?.accessToken,
+      );
+  };
   return (
-    <TouchableOpacity
-      style={{
-        borderTopWidth: ms(0.7),
-        // borderBottomColor: 'gray',
-        //light mode
-        // borderTopColor:'#D8D8D8',
-        //dark mode
-        borderTopColor: '#444444',
-        minHeight: mvs(60),
-        backgroundColor:
-          (unread && colors.unreadBackgroundColor) || colors.primaryColor,
-        width: '100%',
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}
-      activeOpacity={0.8}
-      onPress={() => {
-        RootNavigation.navigate('Chat', {
-          chatHeaderTitle: Name,
-          teamId: item?._id,
-          channelType: item?.type,
-          userId:
-            item?.userIds[0] != props?.userInfoState?.user?.id
-              ? item?.userIds[0]
-              : item?.userIds[1],
-        });
-        props?.setActiveChannelTeamIdAction(item?._id);
-        props?.channelsState?.teamIdAndUnreadCountMapping?.[item?._id] > 0 &&
-          resetUnreadCountAction(
-            props?.orgsState?.currentOrgId,
-            props?.userInfoState?.user?.id,
-            item?._id,
-            props?.userInfoState?.accessToken,
-          );
-      }}>
+    <TouchableItem
+      onPress={onPress}
+      background={
+        Platform.OS === 'android' ? TouchableNativeFeedback.Ripple() : null
+      }
+      activeOpacity={0.8}>
       <View
         style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          padding: ms(13),
+          borderTopWidth: ms(0.7),
+          borderTopColor: '#444444',
+          minHeight: mvs(60),
+          backgroundColor:
+            (unread && colors.unreadBackgroundColor) || colors.primaryColor,
+          width: '100%',
+          flexDirection: 'column',
+          justifyContent: 'center',
         }}>
         <View
-          style={{flexDirection: 'row', alignItems: 'center', maxWidth: '80%'}}>
-          <Icon name={iconName} size={14} color={colors.textColor} />
-          <Text>{'  '}</Text>
-          <Text
-            style={{
-              fontSize: ms(16),
-              fontWeight: unread ? '700' : '400',
-              color: colors.textColor,
-            }}>
-            {Name}
-          </Text>
-        </View>
-        {props?.channelsState?.teamIdAndUnreadCountMapping?.[item?._id] > 0 && (
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            padding: ms(13),
+          }}>
           <View
             style={{
-              backgroundColor: '#73e1ff',
-              paddingHorizontal: ms(5),
-              paddingVertical: mvs(2),
-              borderRadius: ms(5),
-              overflow: 'hidden',
+              flexDirection: 'row',
+              alignItems: 'center',
+              maxWidth: '80%',
             }}>
+            <Icon name={iconName} size={14} color={colors.textColor} />
             <Text
               style={{
-                color: 'black',
-                fontSize: ms(11),
-                fontWeight: 'bold',
-                textAlign: 'center',
-                minWidth: ms(15),
-                height: ms(20),
-                lineHeight: ms(20),
+                fontSize: ms(16),
+                fontWeight: unread ? '700' : '400',
+                color: colors.textColor,
+              }}>{`  ${Name}`}</Text>
+          </View>
+          {props?.channelsState?.teamIdAndUnreadCountMapping?.[item?._id] >
+            0 && (
+            <View
+              style={{
+                backgroundColor: '#73e1ff',
+                paddingHorizontal: ms(5),
+                paddingVertical: mvs(2),
+                borderRadius: ms(5),
                 overflow: 'hidden',
               }}>
-              {props?.channelsState.teamIdAndUnreadCountMapping?.[item?._id]}
-            </Text>
-          </View>
-        )}
-        {/* {unread && (
-          <View>
-            <Text
-              style={{
-                color: '#ff6347',
-                paddingHorizontal: ms(6),
-                paddingVertical: mvs(2),
-                borderRadius: ms(1),
-              }}>
-              New {props?.channelsState.teamIdAndUnreadCountMapping>0 && props?.channelsState.teamIdAndUnreadCountMapping}
-            </Text>
-          </View>
-        )} */}
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: ms(11),
+                  fontWeight: 'bold',
+                  textAlign: 'center',
+                  minWidth: ms(15),
+                  height: ms(20),
+                  lineHeight: ms(20),
+                  overflow: 'hidden',
+                }}>
+                {props?.channelsState.teamIdAndUnreadCountMapping?.[item?._id]}
+              </Text>
+            </View>
+          )}
+        </View>
       </View>
-    </TouchableOpacity>
+    </TouchableItem>
   );
 };
 const SearchChannelCard = ({
@@ -148,74 +148,84 @@ const SearchChannelCard = ({
     ? props?.channelsState?.userIdAndTeamIdMapping[item?._source?.userId]
     : item?._id;
   const iconName = item?._source?.type == 'U' ? 'user' : 'hashtag';
+  const onPress = () => {
+    if (teamId == undefined) {
+      props?.createDmChannelAction(
+        props?.userInfoState?.accessToken,
+        props?.orgsState?.currentOrgId,
+        Name,
+        item?._source?.userId,
+      );
+    }
+    setsearchValue('');
+    navigation.navigate('Chat', {
+      chatHeaderTitle: Name,
+      teamId: teamId,
+      reciverUserId: item?._source?.userId,
+      channelType: item?._source?.type == 'U' ? 'DIRECT_MESSAGE' : 'CHANNEL',
+      userId: item?._source?.userId,
+    });
+  };
   return (
-    <TouchableOpacity
-      style={{
-        borderTopWidth: ms(0.7),
-        borderTopColor: '#444444',
-        minHeight: mvs(60),
-        borderRadius: ms(5),
-        height: s(60),
-        width: '100%',
-        flexDirection: 'column',
-        justifyContent: 'center',
-      }}
-      onPress={() => {
-        if (teamId == undefined) {
-          props?.createDmChannelAction(
-            props?.userInfoState?.accessToken,
-            props?.orgsState?.currentOrgId,
-            Name,
-            item?._source?.userId,
-          );
-        }
-        setsearchValue('');
-        navigation.navigate('Chat', {
-          chatHeaderTitle: Name,
-          teamId: teamId,
-          reciverUserId: item?._source?.userId,
-          channelType:
-            item?._source?.type == 'U' ? 'DIRECT_MESSAGE' : 'CHANNEL',
-          userId: item?._source?.userId,
-        });
-      }}>
+    <TouchableItem onPress={onPress} activeOpacity={0.8}>
       <View
         style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-          padding: ms(13),
+          borderTopWidth: ms(0.7),
+          borderTopColor: '#444444',
+          minHeight: mvs(60),
+          borderRadius: ms(5),
+          height: s(60),
+          width: '100%',
+          flexDirection: 'column',
+          justifyContent: 'center',
         }}>
-        <Icon name={iconName} color={colors.textColor} />
-        <Text
+        <View
           style={{
-            fontSize: ms(16),
-            fontWeight: '400',
-            color: colors.textColor,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            padding: ms(13),
           }}>
-          {' '}
-          {Name}
-        </Text>
-        <View style={{position: 'absolute', left: undefined, right: ms(20)}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              maxWidth: '80%',
+            }}>
+            <Icon name={iconName} color={colors.textColor} />
+            <Text
+              style={{
+                fontSize: ms(16),
+                fontWeight: '400',
+                color: colors.textColor,
+              }}>{`  ${Name}`}</Text>
+          </View>
           {item?._source?.type == 'U' && (
-            <Button
-              title="Profile"
-              onPress={async () => {
-                await searchUserProfileAction(
-                  item?._source?.userId,
-                  userInfoState?.accessToken,
-                );
-                navigation.navigate('UserProfiles', {
-                  displayName:
-                    orgsState?.userIdAndDisplayNameMapping[
-                      item?._source?.userId
-                    ],
-                });
-              }}></Button>
+            <View
+              style={{
+                position: 'absolute',
+                right: ms(20),
+              }}>
+              <Button
+                title="Profile"
+                onPress={async () => {
+                  await searchUserProfileAction(
+                    item?._source?.userId,
+                    userInfoState?.accessToken,
+                  );
+                  navigation.navigate('UserProfiles', {
+                    displayName:
+                      orgsState?.userIdAndDisplayNameMapping[
+                        item?._source?.userId
+                      ],
+                  });
+                }}
+              />
+            </View>
           )}
         </View>
       </View>
-    </TouchableOpacity>
+    </TouchableItem>
   );
 };
 const UsersToAddCard = ({item, setUserIds, userIds, setsearchedUser}) => {
