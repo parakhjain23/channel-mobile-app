e7c86151c5756fb0559489b629082af39ff1a5f7
Parakh <parakhjain2301@gmail.com>
2023-04-24 16:46:00 +0530

    added sentry dsn to env file

--- Channel/AppSentry.js
+++ Channel/AppSentry.js
@@ -7,7 +7,7 @@ import ErrorScreen from './src/screens/errorScreen/ErrorScreen';
 const AppSentry = () => {
   useEffect(() => {
     Sentry.init({
-      dsn: 'https://b3b078bd820c4f6cb1e948fd23c46d59@o4504117127348224.ingest.sentry.io/4505068327075840',
+      dsn: process.env.SENTRY_DSN,
       // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
       // We recommend adjusting this value in production.
       tracesSampleRate: 1.0,
