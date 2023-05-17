1e91b2473d4f77935834412d594417bbc8fb005c
rudrakachhawa <rudrakshkachhawa@gmail.com>
2023-05-17 17:46:48 +0530

    replaced web view player from chat screen and using own player

--- Channel/src/screens/chatScreen/ChatScreen.js
+++ Channel/src/screens/chatScreen/ChatScreen.js
@@ -55,6 +55,7 @@ import {
 } from '../../redux/actions/channelActivities/inviteUserToChannelAction';
 import {ACTIVITIES} from '../../constants/Constants';
 import ScrollDownButton from '../../components/ScrollDownButton';
+import AudioRecordingPlayer from '../../components/AudioRecorderPlayer';
 
 const ChatScreen = ({
   route,
@@ -638,21 +639,7 @@ const ChatScreen = ({
                         flex: 1,
                         alignItems: 'center',
                       }}>
-                      <WebView
-                        source={{
-                          html: `
-                              <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=3, user-scalable=yes">
-                              <style>
-                                body, html { margin: 0; padding: 0; flex:1; background-color: ${colors?.primaryColor};}
-                                audio { width: 100%;}
-                              </style>
-                              <audio controls>
-                              <source src="${audioDataUrl}" type="audio/mp4">
-                                </audio>
-                               `,
-                        }}
-                        style={{flex: 1}}
-                      />
+                    <AudioRecordingPlayer remoteUrl={audioDataUrl} />
                     </View>
                     <MaterialIcons
                       name="cancel"
