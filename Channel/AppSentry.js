7e12911f5a05137158f27d35f3f17c52254a8faa
Parakh <parakhjain2301@gmail.com>
2023-05-29 11:26:26 +0530

    uxcamm move to appsentry

--- Channel/AppSentry.js
+++ Channel/AppSentry.js
@@ -3,6 +3,7 @@ import * as Sentry from '@sentry/react-native';
 import ErrorBoundary from 'react-native-error-boundary';
 import App from './App';
 import ErrorScreen from './src/screens/errorScreen/ErrorScreen';
+import RNUxcam from 'react-native-ux-cam';
 
 const AppSentry = () => {
   useEffect(() => {
@@ -12,9 +13,25 @@ const AppSentry = () => {
       // We recommend adjusting this value in production.
       tracesSampleRate: 1.0,
     });
+    EnableUXCam();
   });
 
   return <ErrorBoundary FallbackComponent={ErrorScreen} children={<App />} />;
 };
 
+async function EnableUXCam() {
+  try {
+    RNUxcam.optIntoSchematicRecordings(); // Add this line to enable iOS screen recordings
+    const configuration = {
+      userAppKey: process.env.UXCAM_APP_KEY,
+      enableAutomaticScreenNameTagging: false,
+      enableAdvancedGestureRecognition: true, // default is true
+      enableImprovedScreenCapture: true, // for improved screen capture on Android
+      // occlusions?: UXCamOcclusion[],
+    };
+    RNUxcam.startWithConfiguration(configuration);
+  } catch (error) {
+    console.log(error, 'error while enabling UXCam');
+  }
+}
 export default Sentry.wrap(AppSentry);
