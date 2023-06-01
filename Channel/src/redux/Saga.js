f8307fa66f3776300f079f1d4e9034ac57057e2c
rudrakachhawa <rudrakshkachhawa@gmail.com>
2023-06-01 17:12:20 +0530

    removed notification saga

--- Channel/src/redux/Saga.js
+++ Channel/src/redux/Saga.js
@@ -1,11 +1,16 @@
-import { all, fork } from "redux-saga/effects";
-import ChannelSaga from "./saga/ChannelSaga";
-import ChatSaga from "./saga/ChatSaga";
-import NotificationSaga from "./saga/NotificationSaga";
-import OrgSaga from "./saga/OrgSaga";
-import SocketSaga from "./saga/SocketSaga";
-import UserSaga from "./saga/UserSaga";
+import {all, fork} from 'redux-saga/effects';
+import ChannelSaga from './saga/ChannelSaga';
+import ChatSaga from './saga/ChatSaga';
+import OrgSaga from './saga/OrgSaga';
+import SocketSaga from './saga/SocketSaga';
+import UserSaga from './saga/UserSaga';
 
 export default function* rootSaga() {
-  yield all([fork(OrgSaga),fork(ChatSaga),fork(UserSaga),fork(ChannelSaga),fork(SocketSaga),fork(NotificationSaga)]);
+  yield all([
+    fork(OrgSaga),
+    fork(ChatSaga),
+    fork(UserSaga),
+    fork(ChannelSaga),
+    fork(SocketSaga),
+  ]);
 }
--- Channel/src/redux/saga/NotificationSaga.js
+++
@@ -1,9 +0,0 @@
-import { takeLatest } from "redux-saga/effects";
-import * as Actions from '../Enums';
-import { getOrgDetails } from "../actions/org/GetOrgDetailsAction";
-
-function* NotificationSaga() {
-    // yield takeLatest(Actions.SET_ACTIVE_CHANNEL_TEAMID)
-}
-
-export default NotificationSaga;
