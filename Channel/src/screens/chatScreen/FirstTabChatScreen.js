3372e6e394d5357bd6eccaa124cec6e7290a2537
Parakh <parakhjain2301@gmail.com>
2023-06-10 16:39:31 +0530

    first Screnn to show color fix

--- Channel/src/screens/chatScreen/FirstTabChatScreen.js
+++ Channel/src/screens/chatScreen/FirstTabChatScreen.js
@@ -1,14 +1,22 @@
 import {View, Text, Image} from 'react-native';
 import React from 'react';
-import { useTheme } from '@react-navigation/native';
+import {useTheme} from '@react-navigation/native';
 
 const FirstTabChatScreen = () => {
-  const {colors} =  useTheme()
+  const {colors} = useTheme();
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor:colors.primaryColor}}>
-        <Image source={require('../../assests/images/appIcon/icon72size.png')}/>
-      <Text>Welcome To channel</Text>
-      <Text>You Are Awesome :)</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.primaryColor,
+      }}>
+      <Image source={require('../../assests/images/appIcon/icon72size.png')} />
+      <Text style={{color: colors?.color, fontSize: 18, fontWeight: '500'}}>
+        Welcome To channel
+      </Text>
+      <Text style={{color: colors?.color}}>You Are Awesome :)</Text>
     </View>
   );
 };
